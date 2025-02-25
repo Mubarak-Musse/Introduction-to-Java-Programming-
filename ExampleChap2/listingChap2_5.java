5029f0ba156cf462625a48b16f19c205b6505073
Mubarak Musse <Mobarak.Musse@gmail.com>
2025-02-26 07:52:31 +1100

    chapter2-soltion

--- ExampleChap2/listingChap2_5.java
+++ ExampleChap2/listingChap2_5.java
@@ -1,9 +1,17 @@
 
+/**
+ *  
+ * adding identifiers to the code
+ * 
+ * */
+
+
 public class listingChap2_5 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		Object j;
+		int k;
+		String  i = (String) (j = k = 2);
+		System.out.println(i + " " + j + " " + k );
+}
+	
 }
